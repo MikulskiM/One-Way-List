b1a96f2c53b963bd5dcc277b2cb701e860bdd6bb
MikulskiM <marek.mikulski.14@gmail.com>
2018-08-20 11:22:20 +0200

    Added: - dodajOsobe() overloading - usunOsobe() ileOsob() returns intiger now everything works smooth and nice :)

--- MojaLista/Source.cpp
+++ MojaLista/Source.cpp
@@ -16,7 +16,9 @@ osoba::osoba() {			// konstruktor struktury "osoba"
 struct lista {
 	osoba *pierwsza;		// wskaŸnik na pocz¹tek listy (pierwszy element listy)
 	void dodajOsobe();		// dodaje osobê na koniec listy
+	void dodajOsobe(std::string imie, std::string nazwisko, unsigned long long pesel);
 	void usunOsobe();
+	int ileOsob();			// funkcja zwracaj¹ca iloœæ osób na naszej liœcie
 	// void sortuj();		// to bedzie funkcja sortuj¹ca wed³ug daty urodzenia (pierwszych 6 cyfr peselu)
 	void wyswietlListe();
 	lista();
@@ -58,10 +60,79 @@ void lista::dodajOsobe() {	// musimy rozpatrzyæ 2 przypadki ( gdy dodajemy pierw
 	}
 }
 
-void lista::usunOsobe() {
+void lista::dodajOsobe(std::string imie, std::string nazwisko, unsigned long long pesel) {
+	osoba *nowa = new osoba;
+	nowa->imie = imie;
+	nowa->nazwisko = nazwisko;
+	nowa->pesel = pesel;
 
-	std::cout << std::endl << "		Deleting person\nnumber of the person you want to delete: ";
-	std::cin
+	if (pierwsza == 0) {
+		pierwsza = nowa;
+	}
+	else {					// przypadek gdy istanieje ju¿ pierwsza osoba na liœcie, wiêc wêdrujemy na koniec listy i tam dodajemy osobê
+		osoba *temp = pierwsza;
+
+		while (temp->nastepna) {	// gdy element posiada wskaŸnik na nastêpny element to wykonuje:
+			temp = temp->nastepna;	// t¹ pêtl¹ dochodzê na koniec listy
+		}
+		temp->nastepna = nowa;
+		nowa->nastepna = 0;			// wskaŸnik na nastêpny element dla ostatniego elementu musi byæ -> NULL lub 0
+	}
+}
+
+int lista::ileOsob() {
+	int licznik=0;
+	osoba *temp = pierwsza;
+	if (pierwsza == 0) {
+		//std::cout << "\tLista jest pusta\n";
+		return 0;
+	}
+	else {
+		while (temp) {
+			licznik++;
+			temp = temp->nastepna;
+		}
+		//std::cout << "\tNa liscie jest " << licznik << " osob\n";
+		return licznik;
+	}
+}
+
+void lista::usunOsobe() {
+	int number;
+	do {
+		std::cout << std::endl << "		Deleting person\nnumber of the person you want to delete (1 = first, 2 = second...): ";
+		std::cin >> number;
+		if (number > ileOsob())
+			std::cout << "\n\n\t cos jest nie tak z numerem ktory wpisales. Chyba jest wiekszy niz liczba osob na liscie\n";
+	} while (number > ileOsob());
+
+	if (number == 1) {
+		pierwsza = pierwsza->nastepna;
+	}
+	else if (number == 0) {
+		std::cout << "Jestes pacyfista? nikogo nie usuniesz\n";
+	}
+	else if (number == ileOsob() ){
+		osoba *temp = pierwsza;
+		int licznik = 1;
+		while (licznik < (ileOsob() - 1)) {
+			temp = temp->nastepna;
+			licznik++;
+		}
+		temp->nastepna = temp->nastepna->nastepna;
+	}
+	else if (number < ileOsob()) {
+		osoba *temp = pierwsza;
+		int licznik = 1;
+		while (licznik != (number - 1)) {
+			temp = temp->nastepna;
+		}
+		temp->nastepna = temp->nastepna->nastepna;
+	}
+	else {
+		std::cout << "\nniezrozumiale polecenie\n";
+	}
+
 
 }
 
@@ -82,9 +153,17 @@ int main() {
 	lista *baza = new lista;		// tworzê listê
 
 	baza->dodajOsobe();
-	baza->dodajOsobe();
-	baza->dodajOsobe();
+	baza->dodajOsobe("Marek", "Mikulski", 97030204971);
+	baza->dodajOsobe("Agata", "Buksinska", 97101812345);
+	baza->dodajOsobe("Karol", "Koala", 89101412345);
+	baza->dodajOsobe("Amelia", "Kot", 98071912345);
 
+	baza->wyswietlListe();
+	std::cout<<"\nNa liscie jest "<<baza->ileOsob()<<" osob";
+
+	baza->usunOsobe();
+
+	std::cout << "\nNa liscie jest " << baza->ileOsob() << " osob";
 	baza->wyswietlListe();
 
 	system("pause");
